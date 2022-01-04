dc4163a1a0f1cecfcbb864fb02b46cfe14ef4a24
Colleen Skaroulis <cskaroulis@yahoo.com>
2022-01-04 09:14:45 -0500

    added dynamic port

--- index.js
+++ index.js
@@ -26,5 +26,5 @@ app.post("/login", (req, res) => {
   res.sendStatus(status);
 });
 
-const port = 5000;
-app.listen(port, () => console.log(`this app is listening on port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`this app is listening on port ${PORT}`));
